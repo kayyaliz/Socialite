d0f26df5a392c36510d806ed859e7bced89cda44
kayyaliz <33028695+kayyaliz@users.noreply.github.com>
2018-06-24 17:02:42 -0400

    NLU GUI first pass
    -spinner working
    -graph working

--- app/src/main/java/edu/wit/mobileapp/socialite/GUI/NLU_GUI.java
+++ app/src/main/java/edu/wit/mobileapp/socialite/GUI/NLU_GUI.java
@@ -1,6 +1,7 @@
 package edu.wit.mobileapp.socialite.GUI;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -16,17 +17,39 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.firebase.ui.auth.AuthUI;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import edu.wit.mobileapp.socialite.Keyboard.R;
 
@@ -36,9 +59,11 @@ public class NLU_GUI extends AppCompatActivity implements NavigationView.OnNavig
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nlu__gui);
+        //TOOLBAR WITH 3 LINES TO OPEN SIDE NAV BAR
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //NAVIGATION DRAWER SETUP START
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.main_content);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -47,6 +72,113 @@ public class NLU_GUI extends AppCompatActivity implements NavigationView.OnNavig
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        //NAVIGATION DRAWER SETUP END
+
+        //SPINNER FOR DATE FIELDS
+        final Spinner dateRangeSpinner = (Spinner) findViewById(R.id.Date_RangeSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.DateRangeArray, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dateRangeSpinner.setAdapter(adapter);
+
+        //RADAR CHART SETUP
+        final PieChart emotionChart = (PieChart) findViewById(R.id.NLU_Emotion_Chart);
+
+        loadViews(emotionChart, 0);
+
+        dateRangeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String dateItemText = dateRangeSpinner.getSelectedItem().toString();
+                Log.v("DateRange", dateItemText);
+                Long startTime = null;
+                Long unixTime = System.currentTimeMillis();
+                Long One_Day_Millis = (24 * 60 * 60 * 1000) * 1L;
+                switch (dateItemText) {
+                    case "All Time":
+                        startTime = 0L;
+                        break;
+                    case "Last 24 Hours":
+                        startTime = unixTime - (1 * One_Day_Millis);
+                        break;
+                    case "Last 7 Days":
+                        startTime = unixTime - (7 * One_Day_Millis);
+                        break;
+                    case "Last 30 Days":
+                        startTime = unixTime - (30 * One_Day_Millis);
+                        break;
+                    case "Last Year":
+                        startTime = unixTime - (365 * One_Day_Millis);
+                        break;
+                    default:
+                        startTime = 0L;
+                }
+                Log.v("STARTTIME", String.valueOf(startTime));
+                loadViews(emotionChart, startTime);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+    }
+
+    private void loadViews(final PieChart emotionChart, long startTime) {
+        final List<PieEntry> entries = new ArrayList<>();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Users").child(user.getUid()).child("NLU_Data");
+
+        // Attach a listener to read the data at our posts reference
+        reference.orderByChild("Timestamp").startAt(startTime).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //Emotions NLU Tracks
+                float Anger = 0f;
+                float Disgust = 0f;
+                float Fear = 0f;
+                float Joy = 0f;
+                float Sadness = 0f;
+                for(DataSnapshot instance: dataSnapshot.getChildren()) {
+                    DataSnapshot data = instance.child("data").child("keywords");
+                    for (DataSnapshot keywords: data.getChildren()) {
+                        Anger += keywords.child("emotion").child("anger").getValue(Float.class);
+                        Disgust += keywords.child("emotion").child("disgust").getValue(Float.class);
+                        Fear += keywords.child("emotion").child("fear").getValue(Float.class);
+                        Joy += keywords.child("emotion").child("joy").getValue(Float.class);
+                        Sadness += keywords.child("emotion").child("sadness").getValue(Float.class);
+                    }
+                }
+                float Total = Anger + Disgust + Fear + Joy + Sadness;
+                Anger = (Anger / Total) * 100;
+                Disgust = (Disgust / Total) * 100;
+                Fear = (Fear / Total) * 100;
+                Joy = (Joy / Total) * 100;
+                Sadness = (Sadness / Total) * 100;
+
+                entries.add(new PieEntry(Anger, "Anger"));
+                entries.add(new PieEntry(Disgust, "Disgust"));
+                entries.add(new PieEntry(Fear, "Fear"));
+                entries.add(new PieEntry(Joy, "Joy"));
+                entries.add(new PieEntry(Sadness, "Sadness"));
+                final int[] MY_COLORS = {Color.RED, Color.YELLOW, Color.MAGENTA,
+                        Color.GREEN, Color.BLUE};
+                ArrayList<Integer> colors = new ArrayList<Integer>();
+
+                for(int c: MY_COLORS) colors.add(c);
+
+                PieDataSet set = new PieDataSet(entries, "Emotion Results");
+                set.setColors(colors);
+                PieData data = new PieData(set);
+                emotionChart.setData(data);
+                emotionChart.invalidate(); // refresh
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
     }
 
     @Override
